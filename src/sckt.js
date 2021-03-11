a55209331682c6ba4bebd16db4812570f9a35017
sydnytt <63668285+sydnytt@users.noreply.github.com>
2021-03-11 09:19:15 +0100

    Update sckt.js

--- src/sckt.js
+++ src/sckt.js
@@ -7,14 +7,52 @@ var mainstring = "";
 var caretstring = ""; 
 var selection;
 var caretmem = ""
-var caret = 12000;
+var caretX = 0, caretY = 0;
 var width, height;
 var socket = io();
 var block;
+var div;
 
+function insertAfter(newNode, existingNode) {
+    existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
+}
+
+function initDiv(str)
+{
+div = new Array(height);
+foreach(element => {element = document.createElement("div");})
+}
+
+
+function addElement () {
+  // create a new div element
+  const newDiv = document.createElement("div");
+
+  // and give it some content
+  const newContent = document.createTextNode("Hi there and greetings!");
+
+  // add the text node to the newly created div
+  newDiv.appendChild(newContent);
+
+  // add the newly created element and its content into the DOM
+  const currentDiv = document.getElementById("div1");
+  document.body.insertBefore(newDiv, currentDiv);
+}
 String.prototype.replaceAt = function(index, char) {
     return this.substr(0, index) + char + this.substr(index + 1);
 }
+var line;
+function initLine()
+{
+line = new Array(height)
+	for(let i=0;i<height;i++)
+	{
+		line[i] = mainstring.substr(i*width, width);
+		div[i].innerHTML = line[i];
+	}
+console.log("init complete:" + pwidth + "x" + "pheight");
+}
+
 document.addEventListener('keypress',  (e) => {
 	var charCode = e.charCode;
 	var char = String.fromCharCode(charCode);
@@ -35,7 +73,7 @@ body.onkeydown = function(e) {
 	if (keyCode == 9)
 	{console.log(caret%width)};
 	if(keyCode == 38 && caret >width-1)
-	{	e.preventDefault(); caret -= width; }
+	{	e.preventDefault(); caretY -= width; }
 	if (keyCode == 40)
 	{
 		if(caret < (width*(height-1))-1){
@@ -72,10 +110,10 @@ function renderCaret()
 	textarea.innerHTML = caretstring;
 }
 
-function insertChar(pos, char)
+function insertChar(x, y, char)
 {
 	mainstring = mainstring.replaceAt(pos, char);
-	renderCaret();
+	if (y == caretY ) renderCaret();
 }
 
 
@@ -89,16 +127,18 @@ socket.onAny((event, ...args) => {
 		{
 			width = parseInt(args[0].width);
 			height = parseInt(args[0].height);
+			
 			block = args[0].block;
 			document.documentElement.style.setProperty('--pwidth', args[0].width + 'ch');
 			mainstring = args[0].alltext;
+			initLine();
 			init_keyevent();
 			renderCaret();
 		}
 		case 'repChar':
 		{
 			
-			insertChar(args[0].pos, args[0].char)
+			insertChar(args[0].x, args[0].y args[0].char)
 			
 		}
 	}
